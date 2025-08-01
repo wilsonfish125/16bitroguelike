a47646f9297ab0a378f4fad98e5833e557f2de77
wilsonfish125 <wil.werbowyj@gmail.com>
2025-08-01 11:23:47 +1000

    Level generation fixed!

---
+++ 00Global/globalItemManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var defaultPool : Array[EquippableItemData] = [
+	preload("res://Items/Resources/Equippable/wIronSword.tres"),
+	# Etc
+]
+
+var forestPool : Array[EquippableItemData] = [
+	preload("res://Items/Resources/Equippable/wKatana.tres"),
+]
--- 00Global/globalMapManager.gd
+++ 00Global/globalMapManager.gd
@@ -1,21 +1,12 @@
 extends Node
 
-# Lots of signals
-
-#signal MapExited(room : Room)
 signal selected(room: Room)
-
-#signal MonsterExited
-#signal ShopExited
-#signal CampfireExited
-#signal TreasureExited
-#signal BossExited
 
 var mapData : Array[Array] = []
 var floorsClimbed : int = 0
 var lastRoom : Room = null
 
-var monsterRooms : Array[String] = [
+const MONSTERROOMS : Array[String] = [
 	"res://Levels/Roguelike/Area1/Enemies/e1a1.tscn", 
 	"res://Levels/Roguelike/Area1/Enemies/e2a1.tscn",
 	"res://Levels/Roguelike/Area1/Enemies/e3a1.tscn",
@@ -45,13 +36,17 @@ var bossRooms : Array[String] = [
 	"res://Levels/Roguelike/Area1/Bosses/b1a1.tscn"
 ]
 
+var monsterRooms : Array[String] = MONSTERROOMS.duplicate()
+
 func _ready() -> void:
 	selected.connect(loadLevel)
 
 func loadLevel( room : Room) -> void:
 	match room.type:
 		Room.Type.MONSTER:
-			LevelManager.loadNewLevel(monsterRooms.pick_random(), "LevelTransition", Vector2.ZERO)
+			var level : String = monsterRooms.pick_random()
+			LevelManager.loadNewLevel(level, "LevelTransition", Vector2.ZERO)
+			monsterRooms.erase(level)
 		Room.Type.SHOP:
 			LevelManager.loadNewLevel(shopRooms.pick_random(), "LevelTransition", Vector2.ZERO)
 		Room.Type.CAMPFIRE:
@@ -61,4 +56,4 @@ func loadLevel( room : Room) -> void:
 		Room.Type.BOSS:
 			LevelManager.loadNewLevel(bossRooms.pick_random(), "LevelTransition", Vector2.ZERO)
 		_:
-			LevelManager.loadNewLevel(monsterRooms.pick_random(), "LevelTransition", Vector2.ZERO)
+			LevelManager.loadNewLevel(MONSTERROOMS.pick_random(), "LevelTransition", Vector2.ZERO)
